889ab6dc4081873b5e6e0de7910937b1d246e857
psssssa <linhphamngoc9@gmail.com>
2017-04-18 19:50:36 +0700

    assigment2 v1

---
+++ INF205_nhom5_assigment2/sourcecode/MvcApplication3/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Data;
+using System.Data.Entity;
+using PagedList;
+
+
+namespace MvcApplication3.Controllers
+{
+    public class HomeController : Controller
+    {
+        private INF205_ASS1_NHOM_5Entities db = new INF205_ASS1_NHOM_5Entities();
+        public ActionResult Index()
+        {
+            ViewBag.Message = "Modify this template to jump-start your ASP.NET MVC application.";
+            var products = db.products.Include
+                (p => p.category).OrderBy(i => i.id).Take(4);
+            return View(products.ToList());
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your app description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
